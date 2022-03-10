459c6be9ef3bb0092673681cf60b2b3ff073d4f6
muadzfathulloh <muadzfathulloh@gmail.com>
2022-03-10 11:07:26 +0700

    Layout 2 - Latihan

--- lib/model/tourism_place.dart
+++ lib/model/tourism_place.dart
@@ -13,14 +13,26 @@ class TourismPlace {
 var tourismPlaceList = [
   TourismPlace(
       name: 'Surabaya Zoo',
-      location: 'Jl. Setail No.1, Darmo',
+      location: 'Jl. Darmo',
       imageAsset: 'assets/images/zoo1.jpg'),
   TourismPlace(
       name: 'Kenjeran Beach',
-      location: 'Jl. Kenjeran, Bulak',
+      location: 'Jl. Kenjeran',
       imageAsset: 'assets/images/beach1.jpg'),
   TourismPlace(
       name: 'Bamboo Forest',
-      location: 'Jl. Raya Marina Asri, Keputih',
+      location: 'Jl. Keputih',
       imageAsset: 'assets/images/bambu1.jpg'),
+  TourismPlace(
+      name: 'Surabaya Heroic Monument',
+      location: 'Jl. Pahlawan',
+      imageAsset: 'assets/images/heroic1.jpg'),
+  TourismPlace(
+      name: 'Bambu Runcing',
+      location: 'Genteng',
+      imageAsset: 'assets/images/runcing1.jpg'),
+  TourismPlace(
+      name: 'Alun-Alun Surabaya',
+      location: 'Genteng',
+      imageAsset: 'assets/images/alun.jpg'),
 ];
